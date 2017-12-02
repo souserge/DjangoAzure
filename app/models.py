0ca9779aa77a0de66be81bee7fe40f0853b87517
Viktor <didenchuk777@gmail.com>
2017-12-02 12:17:39 +0100

    Fix database

--- app/models.py
+++ app/models.py
@@ -3,15 +3,20 @@ Definition of models.
 """
 
 from django.db import models
+GENDER_CHOICE = (('m', 'M'), ('f', 'F'))
+CHARACTER_CHOICE = (('calm', 'Calm'), ('friendly', 'Friendly'), ('defender', 'Defender'), ('hunter', 'Hunter'), ('playful', 'playful'))
+TYPE_CHOICE = (('dog', 'Dog'), ('cat', 'Cat'))
+
 class Pet(models.Model):
     name = models.CharField(max_length=100)
-    type = models.CharField(max_length=100)
-    breed = models.CharField(max_length=100)
-    sex = models.CharField(max_length=100)
-    character = models.CharField(max_length=100)
-    data_of_birth = models.CharField(max_length=100)
+    type = models.CharField(max_length=10, choices=TYPE_CHOICE)
+    breed = models.CharField(max_length=50)
+    sex = models.CharField(max_length=1, choices=GENDER_CHOICE)
+    character = models.CharField(max_length=10, choices=CHARACTER_CHOICE)
+    date_of_birth = models.DateField()
     condition = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
+    contact_person_id = models.CharField(max_length=100)
     photo = models.FileField()
 
     def __str__(self):
