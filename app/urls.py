6f5b886fc5f3bf5c0bc48fbf60cc28264a837d14
Serge Korzh <serge.korzh@gmail.com>
2017-12-02 13:50:32 +0100

    Add additional debug settings

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,8 @@ Definition of urls for DjangoApp.
 """
 from datetime import datetime
 from django.conf.urls import include, url
+from django.conf import settings
+from django.views.static import serve
 from django.contrib.auth.views import *
 from . import views
 
@@ -11,3 +13,10 @@ urlpatterns = [
     url(r'^jsson/$', views.jsson, name='t'),
     url(r'^get_pets', views.get_pets, name='get_pets'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve,
+        {'document_root': settings.MEDIA_ROOT}),
+    ]
